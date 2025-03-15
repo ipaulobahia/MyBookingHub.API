e2997cef23af77e170bba7303ed3d957fdb6cb95
Paulo Bahia <pauloipatinga22@gmail.com>
2025-03-15 10:56:25 -0300

    feat: Adicionar validação de configurações de ambiente, conexão com o Prisma e documentação Swagger.
    - Remover teste de controle de raiz que retorna "Hello World!".
    - Remova o arquivo do controlador de aplicativo.
    - Adicione validação de configurações de ambiente e integração com o módulo Prisma no AppModule.
    - Remover serviço AppService desnecessário.
    - Adicione arquivo de configuração com dados sensíveis.
    - Adicione tipos de ambiente e variáveis de configuração para diferentes ambientes de desenvolvimento.
    - Adicione a configuração do Swagger para documentar a API.
    - Adicione o módulo Prisma com o serviço PrismaService sendo injetável globalmente.
    - Adicionar classe de serviço Prisma para gerenciar conexão com o banco de dados.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import config from './config';
+import { enviroments, Environment } from './environments';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV as Environment] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        JWT_REFRESH_SECRET: Joi.string().required(),
+        ACCESS_TOKEN_EXPIRATION: Joi.string().required(),
+        REFRESH_TOKEN_EXPIRATION: Joi.string().required(),
+      }),
+      validationOptions: { abortEarly: true },
+    }),
+    PrismaModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
